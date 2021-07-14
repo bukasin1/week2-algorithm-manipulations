8eb067239ee083d1f319e826e929742d3aa1df66
Ebuka <bukasin1@gmail.com>
2021-07-14 03:43:14 +0100

    Almost done, before refactoring

--- src/classifier.js
+++ src/classifier.js
@@ -1,9 +1,137 @@
+//const { noOfGroups } = require("../fixtures/outputs/output");
+
 /**
  * This is the entry point to the program
  * Question 1 - Classifier
  *
  * @param {any} input Array of student objects
  */
-function classifier(input) {}
+ const input = [
+    {
+      name: 'Hendrick',
+      dob: '1853-07-18T00:00:00.000Z',
+      regNo: '041',
+    },
+    {
+      name: 'Albert',
+      dob: '1879-03-14T00:00:00.000Z',
+      regNo: '033',
+    },
+    {
+      name: 'Marie',
+      dob: '1867-11-07T00:00:00.000Z',
+      regNo: '024',
+    },
+    {
+      name: 'Neils',
+      dob: '1885-10-07T00:00:00.000Z',
+      regNo: '02',
+    },
+    {
+      name: 'Max',
+      dob: '1858-04-23T00:00:00.000Z',
+      regNo: '014',
+    },
+    {
+      name: 'Erwin',
+      dob: '1887-08-12T00:00:00.000Z',
+      regNo: '09',
+    },
+    {
+      name: 'Auguste',
+      dob: '1884-01-28T00:00:00.000Z',
+      regNo: '08',
+    },
+    {
+      name: 'Karl',
+      dob: '1901-12-05T00:00:00.000Z',
+      regNo: '120',
+    },
+    {
+      name: 'Louis', //
+      dob: '1892-08-15T00:00:00.000Z',
+      regNo: '022',
+    },
+    {
+      name: 'Arthur',
+      dob: '1892-09-10T00:00:00.000Z',
+      regNo: '321',
+    },
+    {
+      name: 'Paul',
+      dob: '1902-08-08T00:00:00.000Z',
+      regNo: '055',
+    },
+    {
+      name: 'William',
+      dob: '1890-03-31T00:00:00.000Z',
+      regNo: '013',
+    },
+    {
+      name: 'Owen',
+      dob: '1879-04-26T00:00:00.000Z',
+      regNo: '052',
+    },
+    {
+      name: 'Martin',
+      dob: '1871-02-15T00:00:00.000Z',
+      regNo: '063',
+    },
+    {
+      name: 'Guye',
+      dob: '1866-10-15T00:00:00.000Z',
+      regNo: '084',
+    },
+    {
+      name: 'Charles',
+      dob: '1868-02-14T00:00:00.000Z',
+      regNo: '091',
+    },
+  ];
 
+
+function classifier(input) {
+    if(!Array.isArray(input)){
+        throw new Error('Input not valid')
+    }
+    inputData = [...input]
+    inputData.map(person => {
+        let dob = new Date(person.dob)
+        let date = new Date(2019, 0, 1)
+        person['age'] = date.getFullYear()-dob.getFullYear()
+        return person
+    })
+    inputData.sort((a,b) => {
+        return a.age-b.age
+    })
+    let groupCount = 0, grouped = []
+    console.log(inputData[0] === inputData[0])
+    console.log(JSON.stringify([1,2]))
+    let output = inputData.reduce((output,person,index) => {
+
+        if(!grouped.includes(person)){
+            groupCount++
+            let filter = inputData.slice(index).filter(item => {
+                return (item.age - person.age) <= 5 && -5<= (item.age - person.age)
+            })
+            let group = filter.length > 3 ? filter.slice(0,3) : filter
+            output['group'+groupCount] = {
+                members:  group,
+                oldest: group.length === 3 ? group[2].age : group.length === 2 ? group[1].age : group[0].age,
+                sum: group.reduce((sum,person) => sum + person.age,0),
+                regNos : group.map(person => +person.regNo).sort((a,b) => a-b)
+            }
+            grouped.push(...(group))
+        }
+
+
+        return output
+    }, {noOfGroups : groupCount})
+    console.log(groupCount)
+    output['noOfGroups'] = groupCount
+
+    return output
+}
+
+console.log(classifier(input))
 module.exports = classifier;
--- src/laundry.js
+++ src/laundry.js
@@ -4,6 +4,43 @@
  *
  * @returns {any} Trip data analysis
  */
-function getMaxPairs(noOfWashes, cleanPile, dirtyPile) {}
+function getMaxPairs(noOfWashes, cleanPile, dirtyPile) {
+    let maxPair = 0
+    cleanPile.sort()
+    dirtyPile.sort()
+    let unmatchedCleanSocks = []
+    while(cleanPile.length){
+        if(cleanPile.includes(cleanPile[0],1)){
+            maxPair++
+            cleanPile.splice(0,2)
+        }else{
+            unmatchedCleanSocks.push(...cleanPile.slice(0,1))
+            cleanPile = cleanPile.slice(1)
+        }
+    }
 
+    for(sock of unmatchedCleanSocks){
+        if(dirtyPile.includes(sock) && noOfWashes > 0){
+            maxPair++
+            let index = dirtyPile.indexOf(sock)
+            dirtyPile.splice(index,1)
+            noOfWashes--
+        }
+    }
+
+    while(dirtyPile.length && noOfWashes >= 2){
+        if(dirtyPile.includes(dirtyPile[0],1)){
+            maxPair++
+            dirtyPile.splice(0,2)
+            noOfWashes-=2
+        }else{
+            dirtyPile = dirtyPile.slice(1)
+        }
+    }
+
+    return maxPair
+}
+
+
+console.log(getMaxPairs(20,[1, 2, 3, 4, 5, 6, 7, 8, 9, 10],[11, 12, 13, 14, 15, 16, 17, 18, 19, 20]))
 module.exports = getMaxPairs;
